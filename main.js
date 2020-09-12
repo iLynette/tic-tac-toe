886a7386ac48d66c16f4605b6e41cfee3d72775b
Lynette <lynetteacholah@icloud.com>
2020-09-12 23:00:06 +0300

    added functionality

--- main.js
+++ main.js
@@ -1,15 +1,54 @@
 function startGame() {
   document.turn = "x";
-
+  setMessage(document.turn + " starts.")
+}
+function setMessage(msg) {
+  document.getElementById("message").innerText = msg;
 }
 function nextMove(square) {
-  square.innerText = document.turn;
+  if(square.innerText == ''){
+    square.innerText = document.turn;
   switchTurn();
+  } else {
+    setMessage("Pick another square.")
+  }
+  
 }
 function switchTurn() {
-  if(document.turn == "x") {
+  if(checkForWinner(document.turn)) {
+    setMessage("congrats " + document.turn + ", you won!")
+  }else if(document.turn == "x") {
     document.turn = "o";
+    setMessage("it's " + document.turn + "'s turn")
   } else {
     document.turn = "x";
+    setMessage("it's " + document.turn + "'s turn")
   }
+  
+}
+
+function checkForWinner(move) {
+  var result = false;
+  if(checkRow(1, 2, 3, move) ||
+    checkRow(4, 5, 6, move) ||
+    checkRow(7, 8, 9, move) ||
+    checkRow(1, 4, 7, move) ||
+    checkRow(2, 5, 8, move) ||
+    checkRow(3, 6, 9, move) ||
+    checkRow(1, 5, 9, move) ||
+    checkRow(3, 5, 7, move)) {
+      result = true;
+    }
+    return result;
+
+}
+function checkRow(a, b, c, move) {
+  var result = false;
+  if(getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+    result = true;
+  }
+  return result
+}
+function getBox(number) {
+  return document.getElementById("s" + number).innerText;
 }
